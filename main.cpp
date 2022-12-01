20159ec042aaed519c2236fda0d697a2b9aa2fdb
sasha <samsha250@gmail.com>
2022-12-01 19:40:28 +0200

    start

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+int main()
+{
+    sf::Window window(
+            sf::VideoMode(640, 480),
+            "Hello World");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event))
+            if (event.type ==
+                sf::Event::Closed)
+                window.close();
+    }
+    return 0;
+}
